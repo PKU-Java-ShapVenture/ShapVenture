8f9fe165b18d7f1cd17662f0600280e554647c2b
guilingfei <127582574+guilingfei@users.noreply.github.com>
2024-05-28 18:30:38 +0800

    Update Zone.java
    分配了shopitem的itemtype对应

--- src/main/java/com/shapventure/Zone.java
+++ src/main/java/com/shapventure/Zone.java
@@ -13,8 +13,10 @@ import javafx.scene.text.Font;
 
 public class Zone {
     
-    //ShopItem内部类给出商店物品的种类、数量、价格参数
-    //天赋格除zoneType不同外，沿用商店的其他方法
+    /* ShopItem内部类给出商店物品的种类、数量、价格参数
+     * 天赋格除zoneType不同外，沿用商店的其他方法
+     * itemType: 0:attack 1:maxhealth 2:health(这里只能回复失去的生命值) 3:maxshield 4:recovery 5:bonusdamagerate 6:armor
+     */
     public class ShopItem {
         protected int itemType, number, cost;
 
