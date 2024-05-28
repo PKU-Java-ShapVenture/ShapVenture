13fa4bd65d393f37b7562648428a969846f8b563
guilingfei <127582574+guilingfei@users.noreply.github.com>
2024-05-28 19:05:26 +0800

    Update Zone.java
    将ShopItem移出

--- src/main/java/com/shapventure/Zone.java
+++ src/main/java/com/shapventure/Zone.java
@@ -4,49 +4,9 @@ import static com.almasb.fxgl.dsl.FXGL.*;
 
 import java.util.Random;
 
-import javafx.beans.binding.Bindings;
-import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
-import javafx.scene.text.Font;
-
 public class Zone {
     
-    /* ShopItem内部类给出商店物品的种类、数量、价格参数
-     * 天赋格除zoneType不同外，沿用商店的其他方法
-     * itemType: 0:attack 1:maxhealth 2:health(这里只能回复失去的生命值) 3:maxshield 4:recovery 5:bonusdamagerate 6:armor
-     */
-    public class ShopItem {
-        protected int itemType, number, cost;
-
-        ShopItem() {
-            itemType = 0;
-            number = 0;
-            cost = 0;
-        }
-
-        ShopItem(int _itemType, int _number, int _cost) {
-            itemType = _itemType;
-            number = _number;
-            cost = _cost;
-        }
-
-        public void set(int _itemType, int _number, int _cost) {
-            itemType = _itemType;
-            number = _number;
-            cost = _cost;
-        }
-
-        public void work() {
-
-        }
-        
-        public void purchase() {
-            getip("money").set(geti("money") - cost);
-            work();
-        }
-    }
+    
 
     protected Type zoneType;
     protected int health, attack;
