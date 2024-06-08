d7ea8bbcb54efe4f933678cbe0e39173df83af5d
PH7th <2367108501@qq.com>
2024-06-08 18:03:45 +0800

    Update Outside.java
    美化

--- src/main/java/com/shapventure/Ouside.java
+++ src/main/java/com/shapventure/Ouside.java
@@ -13,352 +13,107 @@ import java.io.IOException;
  * 数值x在等级y的具体量由LV2data(x,y)给出
  * 本类在src/main文件夹中创建outsideDataSave文本文件，通过字节流以固定顺序保存各类数值局外等级，彼此不隔开
  */
-public class Ouside {
+public class Outside {
     final File f = new File("../../../outsideDataSave.txt");
 
     //请勿更改此顺序
-    int maxhealthLV,
-            maxshieldLV,
-            recoveryLV,
-            attackLV,
-            bonusdamagerateLV,
-            armorLV,
-            moneyLV;
-    int expRequired[] = new int[] { 10, 50, 100, 500, 1000 };
+    private int[] level = new int[7];
+    private int expRequired[] = new int[] { 10, 50, 100, 500, 1000 };
+    private int[][] data = {
+            { 100, 150, 250, 500, 1000, 1500 }, // maxhealth
+            { 30, 80, 150, 200, 300, 500 }, // maxshield
+            { 10, 15, 25, 50, 100, 150 }, // recovery
+            { 10, 15, 25, 50, 100, 150 }, // attack
+            { 5, 10, 15, 25, 35, 50 }, // bonusdamagerate
+            { 1, 2, 5, 8, 15, 30 }, // armor
+            { 0, 20, 50, 100, 180, 360 } // money
+    };
 
-    public Ouside() {
+    public Outside() {
 
     }
 
     public int LV2data(Options option, int LV) {
-        switch (option) {
-            case maxhealth:
-                switch (LV) {
-                    case 0:
-                        return 100;
-                    case 1:
-                        return 150;
-                    case 2:
-                        return 250;
-                    case 3:
-                        return 500;
-                    case 4:
-                        return 1000;
-                    case 5:
-                        return 1500;
+        return data[option.toi()][LV];
+    }
 
-                    default:
-                        return 1;
-                }
-            case maxshield:
-                switch (LV) {
-                    case 0:
-                        return 30;
-                    case 1:
-                        return 80;
-                    case 2:
-                        return 150;
-                    case 3:
-                        return 200;
-                    case 4:
-                        return 300;
-                    case 5:
-                        return 500;
-
-                    default:
-                        return 1;
-                }
-            case recovery:
-                switch (LV) {
-                    case 0:
-                        return 10;
-                    case 1:
-                        return 15;
-                    case 2:
-                        return 25;
-                    case 3:
-                        return 50;
-                    case 4:
-                        return 100;
-                    case 5:
-                        return 150;
-
-                    default:
-                        return 1;
-                }
-            case attack:
-                switch (LV) {
-                    case 0:
-                        return 10;
-                    case 1:
-                        return 15;
-                    case 2:
-                        return 25;
-                    case 3:
-                        return 50;
-                    case 4:
-                        return 100;
-                    case 5:
-                        return 150;
-
-                    default:
-                        return 1;
-                }
-            case bonusdamagerate:
-                switch (LV) {
-                    case 0:
-                        return 5;
-                    case 1:
-                        return 10;
-                    case 2:
-                        return 15;
-                    case 3:
-                        return 25;
-                    case 4:
-                        return 35;
-                    case 5:
-                        return 50;
-
-                    default:
-                        return 1;
-                }
-            case armor:
-                switch (LV) {
-                    case 0:
-                        return 1;
-                    case 1:
-                        return 2;
-                    case 2:
-                        return 5;
-                    case 3:
-                        return 8;
-                    case 4:
-                        return 15;
-                    case 5:
-                        return 30;
-
-                    default:
-                        return 1;
-                }
-            case money:
-                switch (LV) {
-                    case 0:
-                        return 0;
-                    case 1:
-                        return 20;
-                    case 2:
-                        return 50;
-                    case 3:
-                        return 100;
-                    case 4:
-                        return 180;
-                    case 5:
-                        return 360;
-
-                    default:
-                        return 1;
-                }
-
-            default:
-                return 2;
+    public void OusideSave() {
+        try (FileOutputStream fop = new FileOutputStream(f, false)) {
+            for (int i = 0; i < 7; i++)
+                fop.write(level[i]);
+            fop.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public void OusideSave() throws IOException {
-        FileOutputStream fop = new FileOutputStream(f, false);
-        fop.write(maxhealthLV);
-        fop.write(maxshieldLV);
-        fop.write(recoveryLV);
-        fop.write(attackLV);
-        fop.write(bonusdamagerateLV);
-        fop.write(armorLV);
-        fop.write(moneyLV);
-        fop.close();
-    }
-
-    public void OusideRead() throws IOException {
-        FileInputStream fip = new FileInputStream(f);
-        int oop[] = new int[6];
-        byte b[] = new byte[4];
-        int n = 0, i = 0;
-        while (n != -1) {
-            n = fip.read(b);
-            oop[i] = b[3];
-            i++;
+    public void OusideRead() {
+        try (FileInputStream fip = new FileInputStream(f)) {
+            for (int i = 0; i < 7; i++) {
+                level[i] = fip.read();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        set(oop[0], oop[1], oop[2], oop[3], oop[4], oop[5], oop[6]);
-        act();
-        fip.close();
     }
 
     public void act() {
-        getip("maxhealth").set(LV2data(Options.maxhealth, maxhealthLV));
-        getip("maxshield").set(LV2data(Options.maxshield, maxshieldLV));
-        getip("recovery").set(LV2data(Options.recovery, recoveryLV));
-        getip("attack").set(LV2data(Options.attack, attackLV));
-        getip("bonusdamagerate").set(LV2data(Options.bonusdamagerate, bonusdamagerateLV));
-        getip("armor").set(LV2data(Options.armor, armorLV));
-        getip("money").set(LV2data(Options.money, moneyLV));
-    }
-
-    private void set(int _maxhealthLV,
-            int _maxshieldLV,
-            int _recoveryLV,
-            int _attackLV,
-            int _bonusdamagerateLV,
-            int _armorLV,
-            int _moneyLV) {
-        maxhealthLV = _maxhealthLV;
-        maxshieldLV = _maxshieldLV;
-        recoveryLV = _recoveryLV;
-        attackLV = _attackLV;
-        bonusdamagerateLV = _bonusdamagerateLV;
-        armorLV = _armorLV;
-        moneyLV = _moneyLV;
+        getip("maxhealth").set(LV2data(Options.maxhealth, level[0]));
+        getip("maxshield").set(LV2data(Options.maxshield, level[1]));
+        getip("recovery").set(LV2data(Options.recovery, level[2]));
+        getip("attack").set(LV2data(Options.attack, level[3]));
+        getip("bonusdamagerate").set(LV2data(Options.bonusdamagerate, level[4]));
+        getip("armor").set(LV2data(Options.armor, level[5]));
+        getip("money").set(LV2data(Options.money, level[6]));
     }
 
     //当能够成功升级时返回true，否则false
     public boolean upgrade(Options option) {
-        switch (option) {
-            case maxhealth:
-                if (geti("exp") < expRequired[maxhealthLV]
-                        || maxhealthLV == 5) {
-                    return false;
-                } else {
-                    getip("exp").set(geti("exp") - expRequired[maxhealthLV]);
-                    maxhealthLV += 1;
-                    break;
-                }
-            case maxshield:
-                if (geti("exp") < expRequired[maxshieldLV]
-                        || maxshieldLV == 5) {
-                    return false;
-                } else {
-                    getip("exp").set(geti("exp") - expRequired[maxshieldLV]);
-                    maxshieldLV += 1;
-                    break;
-                }
-            case recovery:
-                if (geti("exp") < expRequired[recoveryLV]
-                        || recoveryLV == 5) {
-                    return false;
-                } else {
-                    getip("exp").set(geti("exp") - expRequired[recoveryLV]);
-                    recoveryLV += 1;
-                    break;
-                }
-            case attack:
-                if (geti("exp") < expRequired[attackLV]
-                        || attackLV == 5) {
-                    return false;
-                } else {
-                    getip("exp").set(geti("exp") - expRequired[attackLV]);
-                    attackLV += 1;
-                    break;
-                }
-            case bonusdamagerate:
-                if (geti("exp") < expRequired[bonusdamagerateLV]
-                        || bonusdamagerateLV == 5) {
-                    return false;
-                } else {
-                    getip("exp").set(geti("exp") - expRequired[bonusdamagerateLV]);
-                    bonusdamagerateLV += 1;
-                    break;
-                }
-            case armor:
-                if (geti("exp") < expRequired[armorLV]
-                        || armorLV == 5) {
-                    return false;
-                } else {
-                    getip("exp").set(geti("exp") - expRequired[armorLV]);
-                    armorLV += 1;
-                    break;
-                }
-            case money:
-                if (geti("exp") < expRequired[moneyLV]
-                        || moneyLV == 5) {
-                    return false;
-                } else {
-                    getip("exp").set(geti("exp") - expRequired[moneyLV]);
-                    moneyLV += 1;
-                    break;
-                }
-
-            default:
-                return false;
+        if (option.toi() == 5
+                || geti("exp") < expRequired[level[option.toi()]])
+            return false;
+        else {
+            getip("exp").set(geti("exp") - expRequired[level[option.toi()]]);
+            level[option.toi()]++;
+            return true;
         }
-        act();
-        return true;
     }
 
     //当能够降级升级时返回true，否则false
     public boolean degrade(Options option) {
-        switch (option) {
-            case maxhealth:
-                if (maxhealthLV == 0) {
-                    return false;
-                } else {
-                    maxhealthLV -= 1;
-                    getip("exp").set(geti("exp") + expRequired[maxhealthLV]);
-                    break;
-                }
-            case maxshield:
-                if (maxshieldLV == 0) {
-                    return false;
-                } else {
-                    maxshieldLV -= 1;
-                    getip("exp").set(geti("exp") + expRequired[maxshieldLV]);
-                    break;
-                }
-            case recovery:
-                if (recoveryLV == 0) {
-                    return false;
-                } else {
-                    recoveryLV -= 1;
-                    getip("exp").set(geti("exp") + expRequired[recoveryLV]);
-                    break;
-                }
-            case attack:
-                if (attackLV == 0) {
-                    return false;
-                } else {
-                    attackLV -= 1;
-                    getip("exp").set(geti("exp") + expRequired[attackLV]);
-                    break;
-                }
-            case bonusdamagerate:
-                if (bonusdamagerateLV == 0) {
-                    return false;
-                } else {
-                    bonusdamagerateLV -= 1;
-                    getip("exp").set(geti("exp") + expRequired[bonusdamagerateLV]);
-                    break;
-                }
-            case armor:
-                if (armorLV == 0) {
-                    return false;
-                } else {
-                    armorLV -= 1;
-                    getip("exp").set(geti("exp") + expRequired[armorLV]);
-                    break;
-                }
-            case money:
-                if (moneyLV == 0) {
-                    return false;
-                } else {
-                    moneyLV -= 1;
-                    getip("exp").set(geti("exp") + expRequired[moneyLV]);
-                    break;
-                }
 
-            default:
-                return false;
+        if (option.toi() == 0)
+            return false;
+        else {
+            level[option.toi()]--;
+            getip("exp").set(geti("exp") - expRequired[level[option.toi()]]);
+            return true;
         }
-        act();
-        return true;
     }
 }
 
 enum Options {
-    maxhealth,maxshield,recovery,attack,bonusdamagerate,armor,money;
+    maxhealth, maxshield, recovery, attack, bonusdamagerate, armor, money;
+
+    public int toi() {
+        switch (this) {
+            case maxhealth:
+                return 0;
+            case maxshield:
+                return 1;
+            case recovery:
+                return 2;
+            case attack:
+                return 3;
+            case bonusdamagerate:
+                return 4;
+            case armor:
+                return 5;
+            case money:
+                return 6;
+
+            default:
+                return -1;
+        }
+    }
 }
